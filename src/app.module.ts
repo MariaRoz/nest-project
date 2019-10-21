592d35ef832f8787def596e3411a82afa886cd18
Mariia Shpura <mshpu@macmini095.softservecom.com>
2019-10-21 18:49:54 +0300

    Add migration and sql crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
